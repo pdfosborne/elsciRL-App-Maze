da615da6983ce0ffb49ee32cbd224917e572db4d
Philip Osborne <pdfosborne@gmail.com>
2025-06-19 15:49:45 +0100

    Engine - Render fix state text split before converting to int for plot

--- environment/engine.py
+++ environment/engine.py
@@ -216,7 +216,7 @@ class Engine:
         ax.plot(self.goal[1], self.goal[0], 'g*', markersize=15)
         
         # Plot current position
-        ax.plot(int(state[1]), int(state[0]), 'ro', markersize=10)
+        ax.plot(int(state.split('_')[1]), int(state.split('_')[0]), 'ro', markersize=10)
         
         plt.grid(True)
         return fig
